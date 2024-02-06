f854f00463a248052168076ad19066af27501f71
circuitapps <125308597+circuitapps@users.noreply.github.com>
2024-02-06 00:36:09 -0800

    Remote controlled RGB LED working
    For REMOTE 1 hardware with 6 buttons, all have been programmed to change the color of one RGB led as needed. This is a simple demo to show how ordinary IR remote controls can be customized to control various devices.

---
+++ attiny85_24012/attiny85_24012.ino
@@ -0,0 +1,186 @@
+/*
+ * SimpleReceiver.cpp
+ *
+ * Demonstrates receiving NEC IR codes with IRremote
+ *
+ *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
+ *
+ ************************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2020-2023 Armin Joachimsmeyer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ ************************************************************************************
+ */
+
+/*
+ * Specify which protocol(s) should be used for decoding.
+ * If no protocol is defined, all protocols (except Bang&Olufsen) are active.
+ * This must be done before the #include <IRremote.hpp>
+ */
+//#define DECODE_DENON        // Includes Sharp
+//#define DECODE_JVC
+//#define DECODE_KASEIKYO
+//#define DECODE_PANASONIC    // alias for DECODE_KASEIKYO
+//#define DECODE_LG
+#define DECODE_NEC          // Includes Apple and Onkyo
+//#define DECODE_SAMSUNG
+//#define DECODE_SONY
+//#define DECODE_RC5
+//#define DECODE_RC6
+
+//#define DECODE_BOSEWAVE
+//#define DECODE_LEGO_PF
+//#define DECODE_MAGIQUEST
+//#define DECODE_WHYNTER
+//#define DECODE_FAST
+
+//#define DECODE_DISTANCE_WIDTH // Universal decoder for pulse distance width protocols
+//#define DECODE_HASH         // special decoder for all protocols
+
+//#define DECODE_BEO          // This protocol must always be enabled manually, i.e. it is NOT enabled if no protocol is defined. It prevents decoding of SONY!
+
+//#define DEBUG               // Activate this for lots of lovely debug output from the decoders.
+
+//#define RAW_BUFFER_LENGTH  180  // Default is 112 if DECODE_MAGIQUEST is enabled, otherwise 100.
+
+#include <Arduino.h>
+
+/*
+ * This include defines the actual pin number for pins like IR_RECEIVE_PIN, IR_SEND_PIN for many different boards and architectures
+ */
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp> // include the library
+
+// DEFINITIONS FOR CIRCUITAPPS REMOTE 1 HARDWARE
+#define ON_OFF_BUTTON 0x12
+#define MODE_SELECT_BUTTON 0x1A
+#define VIDEO_SELECT_BUTTON 0x1B
+#define LEFT_BUTTON 0x04
+#define MENU_BUTTON 0x05
+#define RIGHT_BUTTON 0x06
+
+#define RED_LED PB4
+#define BLUE_LED PB1
+#define GREEN_LED PB3
+
+void setup() {
+    Serial.begin(115200);  // Only needed if you want to print stuff to serial terminal (e.g., by using TeraTerm) for test & debug
+    
+    // Just to know which program is running on my Arduino
+    //Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+
+    Serial.print(F("Ready to receive IR signals of protocols: "));
+    printActiveIRProtocols(&Serial);
+    Serial.println(F("at pin " STR(IR_RECEIVE_PIN)));
+
+    pinMode(RED_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+}
+
+void loop() {
+    /*
+     * Check if received data is available and if yes, try to decode it.
+     * Decoded result is in the IrReceiver.decodedIRData structure.
+     *
+     * E.g. command is in IrReceiver.decodedIRData.command
+     * address is in command is in IrReceiver.decodedIRData.address
+     * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
+     */
+    if (IrReceiver.decode()) {
+
+        /*
+         * Print a short summary of received data
+         */
+        IrReceiver.printIRResultShort(&Serial);
+        IrReceiver.printIRSendUsage(&Serial);
+        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+            Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
+            // We have an unknown protocol here, print more info
+            IrReceiver.printIRResultRawFormatted(&Serial, true);
+        }
+        Serial.println();
+
+        /*
+         * !!!Important!!! Enable receiving of the next value,
+         * since receiving has stopped after the end of the current received data packet.
+         */
+        IrReceiver.resume(); // Enable receiving of the next value
+
+        /*
+         * Finally, check the received data and perform actions according to the received command
+         */
+        if (IrReceiver.decodedIRData.command == ON_OFF_BUTTON)
+        {// TURNS OFF ALL LEDs
+            Serial.println(F("User pressed On/Off button"));
+            digitalWrite(RED_LED, LOW);
+            digitalWrite(BLUE_LED, LOW);
+            digitalWrite(GREEN_LED, LOW);
+        }
+        else if (IrReceiver.decodedIRData.command == MODE_SELECT_BUTTON)
+        {// GENERATES TEAL LED COLOR
+            Serial.println(F("User pressed Mode select button"));
+            digitalWrite(RED_LED, LOW);
+            digitalWrite(BLUE_LED, HIGH);
+            digitalWrite(GREEN_LED, HIGH);
+        }
+        else if (IrReceiver.decodedIRData.command == VIDEO_SELECT_BUTTON)
+        {// GENERATES ORANGE LED COLOR
+            Serial.println(F("User pressed Video select button"));
+            digitalWrite(RED_LED, HIGH);
+            digitalWrite(BLUE_LED, LOW);
+            digitalWrite(GREEN_LED, HIGH);
+        }
+        else if (IrReceiver.decodedIRData.command == LEFT_BUTTON)
+        {// GENERATES RED LED COLOR
+            Serial.println(F("User pressed Left button"));
+            digitalWrite(RED_LED, HIGH);
+            digitalWrite(BLUE_LED, LOW);
+            digitalWrite(GREEN_LED, LOW);
+        }
+        else if (IrReceiver.decodedIRData.command == RIGHT_BUTTON)
+        {// GENERATES BLUE LED COLOR
+            Serial.println(F("User pressed Right button"));
+            digitalWrite(RED_LED, LOW);
+            digitalWrite(BLUE_LED, HIGH);
+            digitalWrite(GREEN_LED, LOW);
+        }
+        else if (IrReceiver.decodedIRData.command == MENU_BUTTON)
+        {// GENERATES GREEN LED COLOR
+            Serial.println(F("User pressed Menu button"));
+            digitalWrite(RED_LED, LOW);
+            digitalWrite(BLUE_LED, LOW);
+            digitalWrite(GREEN_LED, HIGH);
+        }
+        else
+        {
+           Serial.println(F("Button NOT recognized!"));
+        }
+
+    }
+}
